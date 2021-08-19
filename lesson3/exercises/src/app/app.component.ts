b1fddbdaa7890ec1a24490fcc361d15598889f44
Minnie Washington <minniewashington@Minnies-MacBook-Pro.local>
2021-08-19 15:11:36 -0500

    finish part of exercise

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,4 +12,67 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+  takeOffEnabled = false;
+
+  handleTakeOff(){
+    let response = confirm('Confirm that the shuttle is ready for takeoff');
+
+    if(response){
+        this.message = "Shuttle in flight."
+        this.color = 'blue'
+        this.height += 10000 
+        this.takeOffEnabled = true
+    }
+  }
+
+  handleLanding(rocketImage){
+    alert('The shuttle is landing. Landing gear engaged.')
+        this.message = "Shuttle has landed."
+        this.color = 'green'
+        this.height = 0
+        rocketImage.style.bottom = '0px';
+        this.takeOffEnabled = false;
+  }
+
+  handleAbort(rocketImage){
+    let abortResponse = confirm('Confirm that you want to abort the mission.');
+
+        if(abortResponse){
+            this.message = "Mission aborted."
+            this.color = 'red'
+            this.height= 0
+            rocketImage.style.bottom = '0px';
+            this.takeOffEnabled = false;
+            
+        }
+  }
+
+  moveRocket(rocketImage, direction){
+    if (direction === 'right') {
+      let movement = parseInt(rocketImage.style.left) + 10 + 'px';
+      rocketImage.style.left = movement;
+      this.width = this.width + 10000;
+    }
+
+   if (direction === 'left') {
+    let movement = parseInt(rocketImage.style.left) - 10 + 'px';
+    rocketImage.style.left = movement;
+    this.width = this.width + 10000;
+    }
+
+    if (direction === 'up') {
+      let movement = parseInt(rocketImage.style.bottom) + 10 + 'px';
+      rocketImage.style.bottom = movement;
+      this.height += 10000;
+      }
+
+    if (direction === 'down') {
+      let movement = parseInt(rocketImage.style.bottom) - 10 + 'px';
+      rocketImage.style.bottom = movement;
+      if(this.height >0){
+      this.height -=10000
+        }
+      }
+  }
+ 
 }
